6efa339e9f1ff1ba5217d2d34d0cd45c79303830
Pablo Rojel <pablorojel@MacBook-Pro-de-ACL-NT280S.local>
2022-04-24 04:04:52 -0400

    cambios

--- servicio.groovy
+++ servicio.groovy
@@ -5,9 +5,19 @@ def job = pipelineJob('test') {
             sandbox()
         }
     }
+def hostsIPsMap = [
+  'QA1': '10.10.10.10',
+  'Q2': '20.20.20.20'
+
+]
+
+def SERVER_DEPLOY=null 
+SERVER_DEPLOY = hostsIPsMap[DESTINO]
+    
     parameters {
     stringParam('perfil_fuse', 'q9-servicio-test', 'Favor ingresar perfil')
     booleanParam('cargarproperties',false, 'indicar si desea cargar archivo de propiedades.')
+    stringParam ('DEPLOY_SERVER')
 
     }
     definition {
