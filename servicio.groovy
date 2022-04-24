da91ce0b0852140985582cd2c2ad768a18bda8fd
Pablo Rojel <pablorojel@MacBook-Pro-de-ACL-NT280S.local>
2022-04-24 07:00:33 -0400

    cambios

--- servicio.groovy
+++ servicio.groovy
@@ -6,10 +6,10 @@ def job = pipelineJob('test') {
         }
     }
     parameters {
-    stringParam('perfil_fuse', 'q9-servicio-test', 'Favor ingresar perfil')
-    booleanParam('cargarproperties',false, 'indicar si desea cargar archivo de propiedades.')
-    choiceParam('${SERVER_DEPLOY}', ['10.10.10.10', '20.20.20.20', 'option 3'])
-    }
+     stringParam('perfil_fuse', 'q9-servicio-test', 'Favor ingresar perfil')
+     booleanParam('cargarproperties',false, 'indicar si desea cargar archivo de propiedades.')
+     choiceParam('SERVER_DEPLOY', ['10.10.10.10', '20.20.20.20', 'option 3'])
+     }
     definition {
         cps {
             script(readFileFromWorkspace('test.jenkinsfile'))
