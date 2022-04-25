9f686407a734b02b9d167aaef318f841e6bddf83
Pablo Rojel <pablorojel@MacBook-Pro-de-ACL-NT280S.local>
2022-04-25 01:57:33 -0400

    cambios

--- servicio.groovy
+++ servicio.groovy
@@ -11,6 +11,12 @@ def job = pipelineJob('test') {
      stringParam('perfil_fuse', 'q9-servicio-test', 'Favor ingresar perfil')
      booleanParam('cargarproperties',false, 'indicar si desea cargar archivo de propiedades.')
      choiceParam('seleccioneAmbiente', ['QA1', 'QA2', 'QA3', 'QA4', 'QA5', 'QA6', 'QA7', 'QA8', 'QA9'])
+     gitParam('Branch') {
+       description 'The Git branch to checkout'
+       type 'BRANCH'
+       defaultValue 'TESTING'
+         }
+     }
      }
 
     definition {
