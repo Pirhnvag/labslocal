e78dd576587c18a298c9c627dcc2a322c4aa4561
Pablo Rojel <pablorojel@MacBook-Pro-de-ACL-NT280S.local>
2022-04-26 00:18:18 -0400

    cambios

--- servicio.groovy
+++ servicio.groovy
@@ -9,20 +9,20 @@ def job = pipelineJob('test') {
     parameters {
       stringParam('perfil_fuse', 'q9-servicio-test', 'Favor ingresar perfil')
       booleanParam('cargarproperties',false, 'Indicar si desea cargar archivo de propiedades.')
-      choiceParam('seleccionarambiente', ['QA1', 'QA2', 'QA3', 'QA4', 'QA5', 'QA6', 'QA7', 'QA8', 'QA9'])
+      choiceParam('seleccionar_ambiente', ['QA1', 'QA2', 'QA3', 'QA4', 'QA5', 'QA6', 'QA7', 'QA8', 'QA9'])
      }
     parameters {
-      gitParam('Branch') {
+      gitParam('branch') {
       description 'La rama de Git a seleccionar'
       type 'BRANCH'
       defaultValue 'testing'
                }
      }
-    definition {
-      cps {
-        script(readFileFromWorkspace('test.jenkinsfile'))
-        sandbox()
-          }
-        }
+    //definition {
+      //cps {
+        //script(readFileFromWorkspace('test.jenkinsfile'))
+        //sandbox()
+          //}
+        //}
      }
 def jobDescription = "job demo jenkins poc"
