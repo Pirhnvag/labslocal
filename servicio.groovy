ab033021415bd0ec81f5ecd5d48a13569b5545f8
Pablo Rojel <pablorojel@MacBook-Pro-de-ACL-NT280S.local>
2022-04-26 00:25:40 -0400

    cambios

--- servicio.groovy
+++ servicio.groovy
@@ -12,7 +12,7 @@ def job = pipelineJob('test') {
       choiceParam('seleccionar_ambiente', ['QA1', 'QA2', 'QA3', 'QA4', 'QA5', 'QA6', 'QA7', 'QA8', 'QA9'])
      }
     parameters {
-      gitParam('branch') {
+      gitParam('Branch') {
       description 'La rama de Git a seleccionar'
       type 'BRANCH'
       defaultValue 'testing'
