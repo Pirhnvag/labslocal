b81d1ecd13d0ba4c289095b8d5681d6db394229f
Pablo Rojel <pablorojel@MacBook-Pro-de-ACL-NT280S.local>
2022-04-26 00:29:52 -0400

    cambios

--- servicio.groovy
+++ servicio.groovy
@@ -9,13 +9,13 @@ def job = pipelineJob('test') {
     parameters {
       stringParam('perfil_fuse', 'q9-servicio-test', 'Favor ingresar perfil')
       booleanParam('cargarproperties',false, 'Indicar si desea cargar archivo de propiedades.')
-      choiceParam('seleccionar_ambiente', ['QA1', 'QA2', 'QA3', 'QA4', 'QA5', 'QA6', 'QA7', 'QA8', 'QA9'])
+      choiceParam('seleccioneAmbiente', ['QA1', 'QA2', 'QA3', 'QA4', 'QA5', 'QA6', 'QA7', 'QA8', 'QA9'])
      }
     parameters {
       gitParam('Branch') {
       description 'La rama de Git a seleccionar'
       type 'BRANCH'
-      defaultValue 'testing'
+      defaultValue ''
                }
      }
     definition {
