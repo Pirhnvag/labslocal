0b4ef1d9a48ce0c046f7acf030b91c663a306a11
Pablo Rojel <pablorojel@MacBook-Pro-de-ACL-NT280S.local>
2022-04-26 03:10:53 -0400

    cambios

--- servicio.groovy
+++ servicio.groovy
@@ -8,7 +8,6 @@ def job = pipelineJob('test') {
      }
     parameters {
       stringParam('perfil_fuse', 'q9-servicio-test', 'Favor ingresar perfil')
-      readOnlyStringParam('perfil_fuse', 'q9-servicio-test', 'Favor ingresar perfil')
       booleanParam('cargarproperties',false, 'Indicar si desea cargar archivo de propiedades.')
       choiceParam('Ambiente', ['QA1', 'QA2', 'QA3', 'QA4', 'QA5', 'QA6', 'QA7', 'QA8', 'QA9'])
      }
