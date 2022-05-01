d5093faa17f9e7b72ba9952c467c82de97e052ea
Pablo Rojel <pablorojel@MacBook-Pro-de-ACL-NT280S.local>
2022-05-01 07:00:10 -0400

    testing-branch

--- servicio.groovy
+++ servicio.groovy
@@ -3,12 +3,12 @@ pipelineJob('job-poc') {
     cps {
       script(readFileFromWorkspace('test.jenkinsfile'))
       sandbox()
-        }
-      }
+    }
+  }
     parameters {
       stringParam('perfil_fuse', 'ingresarambiente-proceso-seguro-venta', 'Favor ingresar perfil')
-      booleanParam('cargarproperties',false, 'Indicar si desea cargar archivo de propiedades.')
-      }
+      booleanParam('cargarproperties', false, 'Indicar si desea cargar archivo de propiedades.')
+    }
     parameters {
       gitParam('Branch') {
       description 'La rama de Git a seleccionar'
@@ -27,21 +27,21 @@ pipelineJob('job-poc') {
         }
       }
     }
-     parameters {
-       activeChoiceReactiveParam('seleccione_servidor') {
-         description('Favor seleccione servidor')
-         filterable()
-         choiceType('RADIO')
-         groovyScript {
-           script('if ( seleccione_ambiente == "Q5") { return ["10.21.61.182", "10.21.61.183", "10.21.61.184"] } else if ( seleccione_ambiente == "Q7") { return ["10.20.73.20", "10.20.73.21", "10.20.73.22"] } else if (states == "Punjab") { return ["Ludhiana", "Amritsar", "Jalandhar"] }')
-           fallbackScript('"fallback choice"')
-        }
-         referencedParameter('seleccione_ambiente')
-         //referencedParameter('BOOLEAN-PARAM-2')
-       }
-     }
+  parameters {
+    activeChoiceReactiveParam('seleccione_servidor') {
+      description('Favor seleccione servidor')
+      filterable()
+      choiceType('RADIO')
+      groovyScript {
+        script('if ( seleccione_ambiente == "Q5") { return ["10.21.61.182", "10.21.61.183", "10.21.61.184"] } else if ( seleccione_ambiente == "Q7") { return ["10.20.73.20", "10.20.73.21", "10.20.73.22"] } else if (states == "Punjab") { return ["Ludhiana", "Amritsar", "Jalandhar"] }')
+        fallbackScript('"fallback choice"')
+      }
+      referencedParameter('seleccione_ambiente')
+    //referencedParameter('BOOLEAN-PARAM-2')
+    }
+  }
     authorization {
         permission('hudson.model.Item.Build', 'q7-profile')
     }
-  }
+}
 
