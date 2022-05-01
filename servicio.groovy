d395420a48ba5937e129b543b731f8d89f68211d
Pablo Rojel <pablorojel@MacBook-Pro-de-ACL-NT280S.local>
2022-05-01 06:14:59 -0400

    testing-branch

--- servicio.groovy
+++ servicio.groovy
@@ -9,6 +9,7 @@ def job = pipelineJob('job-poc') {
     parameters {
       stringParam('perfil_fuse', 'ingresarambiente-proceso-seguro-venta', 'Favor ingresar perfil')
       booleanParam('cargarproperties',false, 'Indicar si desea cargar archivo de propiedades.')
+      choiceParam('Ambiente', ['QA1', 'QA2', 'QA3', 'QA4', 'QA5', 'QA6', 'QA7', 'QA8', 'QA9'])
      }
     parameters {
       gitParam('Branch') {
@@ -34,7 +35,7 @@ def job = pipelineJob('job-poc') {
             filterable()
             choiceType('RADIO')
             groovyScript {
-                script('if ( seleccione_ambiente == "Q5") { return ["10.21.61.182", "10.21.61.183", "10.21.61.184"] } else if ( seleccione_ambiente == "Q7") { return ["10.20.73.20", "10.20.73.21", "10.20.73.22"] } ')
+                script('if ( seleccione_ambiente == "Q5") { return ["10.21.61.182", "10.21.61.183", "10.21.61.184"] } else if ( seleccione_ambiente == "Q7") { return ["10.20.73.20", "10.20.73.21", "10.20.73.22"] } else if (states == "Punjab") { return ["Ludhiana", "Amritsar", "Jalandhar"] }')
                 fallbackScript('"fallback choice"')
             }
             referencedParameter('seleccione_ambiente')
