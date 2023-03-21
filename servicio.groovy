94da5bb4b8e8b0547ae70bdf317c52eb524fe651
Pirhnvag <101233860+Pirhnvag@users.noreply.github.com>
2023-03-21 01:02:29 -0300

    Update servicio.groovy

--- servicio.groovy
+++ servicio.groovy
@@ -5,43 +5,4 @@ pipelineJob('job-poc') {
       sandbox()
     }
   }
-    parameters {
-      stringParam('perfil_fuse', 'ingresarambiente-proceso-seguro-venta', 'Favor ingresar perfil')
-      booleanParam('cargarproperties', false, 'Indicar si desea cargar archivo de propiedades.')
-    }
-    parameters {
-      gitParam('Branch') {
-      description 'La rama de Git a seleccionar'
-      type 'BRANCH'
-      defaultValue 'testing'
-      }
-    }
-    parameters {
-      activeChoiceParam('seleccione_ambiente') {
-        description('Favor seleccione ambiente')
-        filterable()
-        choiceType('RADIO')
-        groovyScript {
-          script('["Q5", "Q7", "Q8", "Q9"]')
-          fallbackScript('"fallback choice"')
-        }
-      }
-    }
-  parameters {
-    activeChoiceReactiveParam('seleccione_servidor') {
-      description('Favor seleccione servidor')
-      filterable()
-      choiceType('RADIO')
-      groovyScript {
-        script('if ( seleccione_ambiente == "Q5") { return ["10.21.61.182", "10.21.61.183", "10.21.61.184"] } else if ( seleccione_ambiente == "Q7") { return ["10.20.73.20", "10.20.73.21", "10.20.73.22"] } ')
-        fallbackScript('"fallback choice"')
-      }
-      referencedParameter('seleccione_ambiente')
-    //referencedParameter('BOOLEAN-PARAM-2')
-    }
-  }
-    authorization {
-        permission('hudson.model.Item.Build', 'q7-profile')
-    }
 }
-
